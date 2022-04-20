e156b3f53358346935ec9ce725c106701c08ace6
Dam Hong Duc <hongduc001h@gmail.com>
2022-04-20 10:01:48 +0700

    add verify email

---
+++ chat_app/lib/providers/authProvider.dart
@@ -0,0 +1,53 @@
+import 'package:chat_app/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthProvider {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  MyUser? _userFromFirebase(User user) {
+    return user != null ? MyUser(userID: user.uid, email: user.email) : null;
+  }
+
+  Future SignIn_WithEmailPasword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? firebaseUser = result.user;
+      return _userFromFirebase(firebaseUser!);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future SignUp_WithEmailPasword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? firebaseUser = result.user;
+      return _userFromFirebase(firebaseUser!);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future ResetPasword(String email) async {
+    try {
+      return await _auth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  Future SignOut(String email) async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // Future isSignIn()
+  // {
+
+  // }
+}
--- chat_app/lib/screens/welcome_screen.dart
+++ chat_app/lib/screens/welcome_screen.dart
@@ -1,10 +1,22 @@
+import 'package:chat_app/providers/authProvider.dart';
 import 'package:chat_app/screens/singn_in_screen.dart';
 import 'package:chat_app/values/app_asstets.dart';
 import 'package:chat_app/values/app_styles.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  // void Check_SignIn() async {
+  //   AuthProvider authProvider = context.read<AuthProvider>();
+  //   bool isSignIn = await authProvider.
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -83,3 +95,86 @@ class WelcomeScreen extends StatelessWidget {
     );
   }
 }
+
+// class WelcomeScreen extends StatelessWidget {
+//   const WelcomeScreen({Key? key}) : super(key: key);
+
+  
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SafeArea(
+//           child: Column(
+//         children: [
+//           Spacer(
+//             flex: 2,
+//           ),
+//           Image.asset(Assets.Welcome),
+//           Spacer(
+//             flex: 1,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             child: Text(
+//               "Welcome to Messengang the best chat app in the world",
+//               maxLines: 2,
+//               textAlign: TextAlign.center,
+//               style: Theme.of(context)
+//                   .textTheme
+//                   .headline5!
+//                   .copyWith(fontWeight: FontWeight.bold, fontSize: 20),
+//             ),
+//           ),
+//           Spacer(
+//             flex: 1,
+//           ),
+//           Text(
+//             "Connect with the people you love",
+//             textAlign: TextAlign.center,
+//             style: TextStyle(
+//               color: Theme.of(context)
+//                   .textTheme
+//                   .bodyText1!
+//                   .color!
+//                   .withOpacity(0.64),
+//             ),
+//           ),
+//           Spacer(
+//             flex: 4,
+//           ),
+//           TextButton(
+//               onPressed: () {
+//                 Navigator.push(context,
+//                     MaterialPageRoute(builder: (context) => SignInScreen()));
+//               },
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Text(
+//                     'Skip',
+//                     style: TextStyle(
+//                       color: Theme.of(context)
+//                           .textTheme
+//                           .bodyText1!
+//                           .color!
+//                           .withOpacity(0.6),
+//                     ),
+//                   ),
+//                   Icon(
+//                     Icons.arrow_forward_ios,
+//                     color: Theme.of(context)
+//                         .textTheme
+//                         .bodyText1!
+//                         .color!
+//                         .withOpacity(0.6),
+//                     size: 16,
+//                     semanticLabel: 'Text to announce in accessibility modes',
+//                   ),
+//                 ],
+//               ))
+//         ],
+//       )),
+//     );
+//   }
+// }
